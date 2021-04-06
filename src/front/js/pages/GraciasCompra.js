5d426573587db82bd142140f30d7be81b602e0d7
Yun205 <yunjoungji@gmail.com>
2021-04-06 20:42:57 +0000

    agergar compra

--- src/front/js/pages/GraciasCompra.js
+++ src/front/js/pages/GraciasCompra.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { Container } from "react-bootstrap";
 
 export const GraciasCompra = props => {
 	const { store, actions } = useContext(Context);
@@ -9,14 +10,16 @@ export const GraciasCompra = props => {
 
 	return (
 		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element:</h1>
-			<hr className="my-4" />
+			<Container>
+				<h1 className="display-4">Gracis por su compra</h1>
+				<hr className="my-4" />
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
+				<Link to="/">
+					<span className="btn btn-primary btn-lg" role="button">
+						Back home
+					</span>
+				</Link>
+			</Container>
 		</div>
 	);
 };
